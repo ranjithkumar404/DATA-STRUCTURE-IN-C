5619866c153d5dd87a4c7eef91bc75e8484fca0c
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-04-07 11:13:20 +0530

    program to sort the entered number using queue

---
+++ infixtopostfix.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MS 5
+char infix[50], postfix[50], item;
+void convert();
+struct stack
+{
+    int top;
+    char item[MS];
+} s;
+void push(char value)
+{
+    if (s.top == (MS - 1))
+    {
+        printf("The stack is overflow\n");
+        exit(0);
+    }
+    else
+    {
+        s.item[++s.top] = value;
+    }
+}
+int pop()
+{
+    if (s.top == -1)
+    {
+        printf("stack underflow\n");
+        exit(0);
+    }
+    return (s.item[s.top--]);
+}
+int empty()
+{
+    if (s.top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int precedence(char c)
+{
+    switch (c)
+    {
+    case '^':
+        return 3;
+    case '*':
+    case '/':
+    case '%':
+        return 2;
+    case '+':
+    case '-':
+        return 1;
+    case '(':
+        return 0;
+    }
+}
+void main()
+{
+    s.top = -1;
+    printf("Enter the infix expression :\n");
+    gets(infix);
+    convert();
+    printf("The postfix expression is:\n");
+    puts(postfix);
+}
+void convert()
+{
+    int i, pos = 0;
+    char symb, t;
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        symb = infix[i];
+        switch (symb)
+        {
+        case '(':
+            push(symb);
+            break;
+        case ')':
+            while ((t = pop()) != '(')
+            {
+                postfix[pos++] = t;
+            }
+            break;
+        case '^':
+        case '*':
+        case '/':
+        case '%':
+        case '+':
+        case '-':
+            while ((!empty()) && ((precedence(s.item[s.top])) >= precedence(symb)))
+            {
+                postfix[pos++] = pop();
+            }
+            push(symb);
+            break;
+        default:
+            postfix[pos++] = symb;
+            break;
+        }
+    }
+    while (!empty())
+    {
+        postfix[pos++] = pop();
+    }
+    postfix[pos] = '\0';
+}
